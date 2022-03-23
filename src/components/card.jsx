38d2560d4213d7d1a31e1404cfee4967005cda3f
André Díaz <andre290800@gmail.com>
2022-03-23 12:41:32 -0600

    feat: add card links

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -1,19 +1,30 @@
 import React from "react";
 import BCard from "react-bootstrap/Card";
 import Badge from "react-bootstrap/Badge";
+import { LinkContainer } from "react-router-bootstrap";
 
 const capitalize = (s) => {
   return s.replace(/^./, (c) => c.toUpperCase());
 };
 
 function Card({ pokemon }) {
+  const base_url = "/pokemon/";
+  const url_name = base_url + pokemon.name;
+  const url_id = base_url + pokemon.id;
+
   return (
     <BCard bg="danger" text="white">
       <BCard.Header>
-        <h5>{capitalize(pokemon.name)}</h5>
-        <span>#{pokemon.id}</span>
+        <LinkContainer to={url_name}>
+          <h5>{capitalize(pokemon.name)}</h5>
+        </LinkContainer>
+        <LinkContainer to={url_id}>
+          <span>#{pokemon.id}</span>
+        </LinkContainer>
       </BCard.Header>
-      <BCard.Img src={pokemon.sprites.front_default} />
+      <LinkContainer to={url_name}>
+        <BCard.Img src={pokemon.sprites.front_default} />
+      </LinkContainer>
       <BCard.Footer>
         {pokemon.types.map((element) => (
           <Badge bg={null} className={"type-" + element.type.name}>
