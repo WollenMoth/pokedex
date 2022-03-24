6f087ffd1af516442f68d1868144bde18ead5446
André Díaz <andre290800@gmail.com>
2022-03-23 23:52:43 -0600

    feat: routes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
+import { Fragment } from "react";
+import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
+import Container from "react-bootstrap/Container";
+import Navbar from "./components/navbar";
+import Home from "./components/home";
 import "./css/App.css";
 
 function App() {
   return (
     <Fragment>
       <ToastContainer />
+      <Navbar />
+      <Container as="main" className="mt-3">
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Container>
     </Fragment>
   );
 }
