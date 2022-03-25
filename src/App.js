e3acb5c283cee4d7e7fd6e57cc4455269c52310b
André Díaz <andre290800@gmail.com>
2022-03-24 22:13:15 -0600

    feat: not found

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import { Fragment } from "react";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import Container from "react-bootstrap/Container";
 import Navbar from "./components/navbar";
 import Home from "./components/home";
+import NotFound from "./components/notFound";
 import "./css/App.css";
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
       <Container as="main" className="mt-3">
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/not-found" element={<NotFound />} />
+          <Route path="*" element={<Navigate to="/not-found" replace />} />
         </Routes>
       </Container>
     </Fragment>
