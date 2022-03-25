83a988342953a8d8d922202214c71a14c06d49ff
André Díaz <andre290800@gmail.com>
2022-03-25 00:02:09 -0600

    refactor: remove navbar links

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Container from "react-bootstrap/Container";
 import Image from "react-bootstrap/Image";
-import BNav from "react-bootstrap/Nav";
 import BNavbar from "react-bootstrap/Navbar";
 import { LinkContainer } from "react-router-bootstrap";
 import logo from "../assets/images/logo.png";
@@ -15,21 +14,13 @@ function Navbar(props) {
       fixed="top"
       className="navbar-inverse"
     >
-      <Container fluid>
+      <Container fluid className="justify-content-center justify-content-md-start">
         <LinkContainer to="/">
           <BNavbar.Brand>
             <Image src={logo} alt="logo" />
             Pokédex
           </BNavbar.Brand>
         </LinkContainer>
-        <BNavbar.Toggle aria-controls="basic-navbar-nav" />
-        <BNavbar.Collapse id="basic-navbar-nav">
-          <BNav className="me-auto">
-            <LinkContainer to="/">
-              <BNav.Link>Home</BNav.Link>
-            </LinkContainer>
-          </BNav>
-        </BNavbar.Collapse>
       </Container>
     </BNavbar>
   );
